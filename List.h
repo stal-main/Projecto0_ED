e34d56f037b20a6e3a4c362fe37c46c65298af45
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-05-05 21:39:22 -0600

    linkedList añadido

---
+++ List.h
@@ -0,0 +1,27 @@
+#pragma once
+
+template<typename E>
+class List {
+public:
+	List() {}
+	List(const List<E>& other) = delete;
+	void operator=(const List<E>& other) = delete;
+	virtual ~List() {}
+	void insert(E element) = 0;
+	void append(E element) = 0;
+	virtual void setElement(E element) = 0;
+	virtual E getElement() = 0;
+	E remove() = 0;
+	virtual void clear() = 0;
+	virtual void goToStart() = 0;
+	virtual void goToEnd() = 0;
+	virtual void goToPos(int pos) = 0;
+	virtual void next() = 0;
+	virtual void previous() = 0;
+	virtual bool atStart() = 0;
+	virtual bool atEnd() = 0;
+	virtual int getPos() = 0;
+	virtual int getSize() = 0;
+	virtual void print() = 0;
+};
+
